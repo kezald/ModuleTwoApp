43464b3b69e72776805bd58d94fa902a6267acf4
Nikolai Kolbenev <kolbenev2011@gmail.com>
2017-07-07 20:32:25 +1200

    Initial Project State

---
+++ FirstApp/FirstApp/AzureManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace FirstApp
+{
+    public class AzureManager
+    {
+        private static AzureManager instance;
+        private MobileServiceClient client;
+        //private IMobileServiceTable<TableModel> table;
+
+        private AzureManager()
+        {
+            this.client = new MobileServiceClient("https://moduletwoapp.azurewebsites.net");
+            //this.table = this.client.GetTable<TableModel>();
+        }
+
+        public MobileServiceClient AzureClient
+        {
+            get { return client; }
+        }
+
+        public static AzureManager AzureManagerInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AzureManager();
+                }
+
+                return instance;
+            }
+        }
+
+        //public async Task<List<TableModel>> GetHotDogInformation()
+        //{
+        //    return await this.table.ToListAsync();
+        //}
+    }
+}
---
+++ FirstApp/FirstApp/TableModel.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace FirstApp
+{
+    class TableModel
+    {
+        [JsonProperty(PropertyName = "Id")]
+        public string ID { get; set; }
+
+        
+    }
+}
